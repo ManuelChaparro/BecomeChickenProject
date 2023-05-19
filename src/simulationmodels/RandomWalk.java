c9d5a6b0aadb5d963ffa3016ca283c343e30af02
DiegoOrtiz05 <macroc222@gmail.com>
2023-05-18 22:11:37 -0500

    Se modifico el random walk para que cumpla los nuevos requisitos

--- src/simulationmodels/RandomWalk.java
+++ src/simulationmodels/RandomWalk.java
@@ -15,23 +15,29 @@ public class    RandomWalk {
     }
 
     public List<Double> generateNumberList(int index,int quantityNumbers){
-        int firstX = (int) Math.round(randomNumberList.get(index == 0 ? index*1:0) * (3 - 1) + 1);
+        int firstX = (int) Math.round(randomNumberList.get(index == 0 ? index*1 :0) * (3 - 1) + 1);
         int myT = (int) Math.round(randomNumberList.get(index * 2) * (5 - 3) + 3);
         int myG = (int) Math.round(randomNumberList.get(index * 3)* (13 - 10) + 10);
         return new MultiplicativeCongruence().generateNumbers(firstX,myT,myG,quantityNumbers);
     }
 
-    public List<Integer> generateStep(int maxLimit,int minLimit, int quantityNumbers,int iceCreamNumber){
+    public List<Integer> generateStep(int minLimit,int maxLimit, int quantityNumbers,int iceCreamNumber,int initialPosition){
         List<Double> myRandomNumberList = generateNumberList(iceCreamNumber,quantityNumbers);
         List<Integer> result = new ArrayList<>();
-        int movement = 0;
+        int position = initialPosition;
         for (int i = 0; i < quantityNumbers; i++) {
-            if(myRandomNumberList.get(i)<0.5){
-                movement+=1;
+            if(position > minLimit && position < maxLimit) {
+                if (myRandomNumberList.get(i) < 0.5) {
+                    position += 25;
+                } else {
+                    position -= 25;
+                }
+            }else if(position >= maxLimit){
+                position -= 25;
             }else{
-                movement-=1;
+                position += 25;
             }
-            result.add(movement);
+            result.add(position);
         }
         return result;
     }
--- src/test/simulationmodel/TestRandomWalk.java
+++ src/test/simulationmodel/TestRandomWalk.java
@@ -2,16 +2,17 @@ package test.simulationmodel;
 
 import simulationmodels.RandomWalk;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TestRandomWalk {
 
     public static void main(String[] args) {
         RandomWalk myRandomWalk = new RandomWalk();
-        List<Integer> list =  myRandomWalk.generateStep(0,1000,5000,1);
+        List<Integer> list =  myRandomWalk.generateStep(0,1000,5000,1,250);
         for (int i = 0; i < list.size(); i++) {
-          System.out.println(list.get(i));
+            System.out.println(list.get(i));
         }
-
     }
 }
